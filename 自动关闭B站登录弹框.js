e60fc5008fb86389dbce5246ed31de7cf6c14f70
wrote-code <developer@noreply.email.com>
2024-03-24 21:31:04 +0800

    修复：B站自动播放的视频关闭登录弹框后无法自动播放

--- 自动关闭B站登录弹框.js
+++ 自动关闭B站登录弹框.js
@@ -9,42 +9,43 @@
 // @grant        none
 // ==/UserScript==
 
-// note 自动播放场景下，关闭登录弹框后会暂停，需要手动播放。
-(function() {
-    'use strict';
-//---------------------
-console.log("执行脚本");
-let id1;
-let id2;
-let id3;
-id1 = setInterval(() => {
-  // 全屏遮罩+屏幕中央登录弹框
-  const dom = document.getElementsByClassName("bili-mini-mask");
-  if (dom != null && dom.length > 0) {
-    dom[0].remove();
-    //clearInterval(id1);
-    document.getElementsByClassName("bpx-player-video-wrap")[0].click();
-    console.log("已移除登录弹框");
-  }
-}, 500)
+(function () {
+  'use strict';
+  //---------------------
 
-id2 = setInterval(() => {
-  // 进度条上方登录提示
-  const dom2 = document.getElementsByClassName("bpx-player-toast-wrap");
-  if (dom2 != null && dom2.length > 0) {
-    dom2[0].remove();
-    //clearInterval(id2);
-    console.log("已移除进度条上方登录提示");
-  }
-}, 500);
+  console.log("执行脚本");
 
-id3 = setInterval(() => {
-  const dom3 = document.getElementsByClassName("bili-circle");
-  if (dom3 != null && dom3.length > 0) {
-    dom3[0].click();
-    //clearInterval(id3);
-    console.log("已移除投票弹框");
-  }
-}, 500);
+  let id1;
+  let id2;
+  let id3;
+  id1 = setInterval(() => {
+    // 全屏遮罩+屏幕中央登录弹框
+    const dom = document.getElementsByClassName("bili-mini-close-icon");
+    if (dom != null && dom.length > 0) {
+      dom[0].click();
+      // clearInterval(id1);
+      document.getElementsByClassName("bpx-player-video-wrap")[0].click();
+      console.log("已移除登录弹框");
+    }
+  }, 500)
+
+  id2 = setInterval(() => {
+    // 进度条上方登录提示
+    const dom2 = document.getElementsByClassName("bpx-player-toast-wrap");
+    if (dom2 != null && dom2.length > 0) {
+      dom2[0].remove();
+      // clearInterval(id2);
+      console.log("已移除进度条上方登录提示");
+    }
+  }, 500);
+
+  id3 = setInterval(() => {
+    const dom3 = document.getElementsByClassName("bili-circle");
+    if (dom3 != null && dom3.length > 0) {
+      dom3[0].click();
+      // clearInterval(id3);
+      console.log("已移除投票弹框");
+    }
+  }, 500);
 
 })();
