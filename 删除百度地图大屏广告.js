b28a5e6edf6d7681e465d0c2e5ad1bd0d64335ff
chen <39870481+cpupg@users.noreply.github.com>
2024-06-23 14:41:16 +0800

    增加百度地图url匹配，现在可以匹配maps和map

--- 删除百度地图大屏广告.js
+++ 删除百度地图大屏广告.js
@@ -5,6 +5,8 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://ditu.baidu.com/*
+// @match        https://maps.baidu.com/*
+// @match        https://map.baidu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
 // @grant        none
 // ==/UserScript==
